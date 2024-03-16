5a3b01c8c1684ffb9566463cee70c0a7a474b2de
Narasimha <narasimhacharithartha@gmail.com>
2024-03-16 15:07:15 +0530

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,80 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+const Main = () => {
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="user-icon" />
+      </div>
+      <div className="main-container">
+
+      {!showResult
+      ?<>
+        <div className="greet">
+          <p><span>Hello, Dev.</span></p>
+          <p>How can i help u today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>The Best places to visit in India.</p>
+            <img src={assets.compass_icon} alt="icon" />
+          </div>
+          <div className="card">
+            <p>What is current GDP of our country?</p>
+            <img src={assets.bulb_icon} alt="icon" />
+          </div>
+          <div className="card">
+            <p>Curret Affairs</p>
+            <img src={assets.message_icon} alt="icon" />
+          </div>
+          <div className="card">
+            <p>Why is infilation hitting middle class people more?</p>
+            <img src={assets.code_icon} alt="icon" />
+          </div>
+        </div>
+      </>
+      :<div className='result'>
+        <div className="result-title">
+          <img src={assets.user_icon} alt="" />
+          <p>{recentPrompt}</p>
+        </div>
+        <div className="result-data">
+          <img src={assets.gemini_icon} alt="" />
+          {loading
+          ?<div className='loader'>
+            <hr />
+            <hr />
+            <hr />
+          </div>
+          :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+          }
+        </div>
+      </div>
+      }
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Enter a promt here' />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null}
+            </div>
+          </div>
+          <p className='bottom-info'>
+            Gemini may display innacurate ifo, including about people. so double-check its responses, Your privacy and Gemini Apps.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
